6e2b8f1b627a30b8f8f56136c0b4f2bc67da2c1c
patrick letourneux <patrick.letourneux20@gmail.com>
2022-08-22 10:24:27 +0200

    methode handle edges done

--- kata/MarsRover.js
+++ kata/MarsRover.js
@@ -1,6 +1,6 @@
 class MarsRover {
-    coordX = 0;
-    coordY = 0;
+    coordX = 1;
+    coordY = 1;
     direction = 'N'; // N,S,E,W
     commandsArrayCharac = [];
     move([...commands]){
@@ -18,6 +18,7 @@ class MarsRover {
         for (let letter of this.commandsArrayCharac){
             // console.log('result ',[this.coordX,this.coordY,this.direction])
             this.advance(letter);
+            this.handleEdges();
             this.turn(letter);
         }
     }
@@ -43,6 +44,16 @@ class MarsRover {
         if (this.direction === 'W'){
             this.coordX -= value;
         }
+
+
+    }
+    handleEdges(){
+        if (this.coordX === 0){
+            this.coordX = 5;
+        }
+        if (this.coordY === 0){
+            this.coordY = 5;
+        }
     }
     turn(letter){
         const directionsR = ['N','E','S','W','N'];
@@ -56,6 +67,7 @@ class MarsRover {
             this.direction = directionsR[index - 1];
         }
     }
+
 }
 
 module.exports = MarsRover;
--- kata/MarsRover.test.js
+++ kata/MarsRover.test.js
@@ -6,38 +6,37 @@ describe('MarsRover class', () => {
     
     beforeEach(() => {
         instance.direction = 'N';
-        instance.coordX = 0;
-        instance.coordY = 0;
-
+        instance.coordX = 1;
+        instance.coordY = 1;
       })
 
     describe('move()', () => {
     
-      test(`move(['f']) return [0,1,'N']`, () => {
-        expect(instance.move(['f'])).toEqual([0,1,'N']);
-      });
-      test(`move(['b']) return [-1,0,'N']`, () => {
-        expect(instance.move(['b'])).toEqual([0,-1,'N']);
-      });
-      test(`move(['f']) return [1,0,'N']`, () => {
-        instance.direction = 'E';
-        expect(instance.move(['f'])).toEqual([1,0,'E']);
+      test(`move(['f']) return [1,2,'N']`, () => {
+        expect(instance.move(['f'])).toEqual([1,2,'N']);
       });
       test(`move(['b']) return [1,0,'N']`, () => {
+        expect(instance.move(['b'])).toEqual([1,5,'N']);
+      });
+      test(`move(['f']) return [2,1,'E']`, () => {
+        instance.direction = 'E';
+        expect(instance.move(['f'])).toEqual([2,1,'E']);
+      });
+      test(`move(['b']) return [2,1,'W']`, () => {
         instance.direction = 'W';
-        expect(instance.move(['b'])).toEqual([1,0,'W']);
+        expect(instance.move(['b'])).toEqual([2,1,'W']);
       });
-      test(`move(['r']) return [0,0,'E']`, () => {
-        expect(instance.move(['r'])).toEqual([0,0,'E']);
+      test(`move(['r']) return [1,1,'E']`, () => {
+        expect(instance.move(['r'])).toEqual([1,1,'E']);
       });
-      test(`move(['l']) return [0,0,'W']`, () => {
-        expect(instance.move(['l'])).toEqual([0,0,'W']);
+      test(`move(['l']) return [1,1,'W']`, () => {
+        expect(instance.move(['l'])).toEqual([1,1,'W']);
       });
-      test(`move(['l','f','f']) return [-2,0,'W']`, () => {
-        expect(instance.move(['l','f','f'])).toEqual([-2,0,'W']);
+      test(`move(['l','f','f']) return [-1,1,'W']`, () => {
+        expect(instance.move(['l','f','f'])).toEqual([4,1,'W']);
       });
-      test(`move(['l','l','f','f']) return [0,-2,'S']`, () => {
-        expect(instance.move(['l','l','f','f'])).toEqual([0,-2,'S']);
+      test(`move(['l','l','f','f']) return [1,-1,'S']`, () => {
+        expect(instance.move(['l','l','f','f'])).toEqual([1,4,'S']);
       });
     
     });
